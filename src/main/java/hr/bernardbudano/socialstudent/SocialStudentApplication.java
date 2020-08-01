6211940fceabb671034a86666c45199c6731aea3
bbudano <bbudano@gmail.com>
2020-08-01 14:52:52 +0200

    Initial commit

---
+++ src/main/java/hr/bernardbudano/socialstudent/SocialStudentApplication.java
@@ -0,0 +1,13 @@
+package hr.bernardbudano.socialstudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialStudentApplication.class, args);
+	}
+
+}
